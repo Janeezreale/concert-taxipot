8d2a4bf499368f3c6f68188330a4c6bacec938e3
junha-afk <noh.junha@gmail.com>
2026-07-08 17:26:47 +0900

    design changes

--- vite.config.ts
+++ vite.config.ts
@@ -12,15 +12,15 @@ export default defineConfig({
           return html;
         }
         return html.replace(
-          '<meta name="viewport" content="width=device-width, initial-scale=1.0" />',
-          `<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />',
+          `<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <script type="text/javascript">
-        (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "xj48863ssz");
-    </script>`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "xj48863ssz");
+    </script>
         );
       },
     },
