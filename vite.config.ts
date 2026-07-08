6bec6b2ff8e1286f6eceb7bc09687a13babdc619
Gabeen Yoon <teukday0701@naver.com>
2026-07-08 22:19:21 +0900

    오타 수정

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,12 @@ export default defineConfig({
           '<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />',
           `<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
+      (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "xj48863ssz");
-    </script>
+      })(window, document, "clarity", "script", "xj48863ssz");
+    </script>`
         );
       },
     },
